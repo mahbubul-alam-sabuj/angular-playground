2bb6bd1f3453904dd1df2da0cbfe6c24339f990e
mahbubsabuj <mahbubul.alam@selise.ch>
2022-11-07 15:40:10 +0600

    update: Add country selection custom control

--- src/app/custom-form-control/components/custom-form/country-selector/country-selector.component.ts
+++ src/app/custom-form-control/components/custom-form/country-selector/country-selector.component.ts
@@ -1,12 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import { ControlValueAccessor } from '@angular/forms';
+import { Component, forwardRef, OnInit, Provider } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
+import { Country } from 'src/app/shared/types/country.interface';
+
+const COUNTRY_CONTROL_VALUE_ACCESSOR: Provider = {
+  provide: NG_VALUE_ACCESSOR,
+  useExisting: forwardRef(() => CountrySelectorComponent),
+  multi: true,
+};
 @Component({
   selector: 'app-country-selector',
   templateUrl: './country-selector.component.html',
   styleUrls: ['./country-selector.component.scss'],
+  providers: [COUNTRY_CONTROL_VALUE_ACCESSOR],
 })
 export class CountrySelectorComponent implements ControlValueAccessor, OnInit {
+  countries: Country[] = [
+    { code: 'IN', name: 'India' },
+    { code: 'US', name: 'United States' },
+    { code: 'GB-ENG', name: 'England' },
+    { code: 'NL', name: 'Netherlands' },
+  ];
+
   disabled = false;
 
   selected: string | null = null;
@@ -22,21 +37,22 @@ export class CountrySelectorComponent implements ControlValueAccessor, OnInit {
   onCountrySelect(value: string) {
     this.selected = value;
     if (this.onTouched) this.onTouched();
+    if (this.onChanged) this.onChanged(value);
   }
 
-  writeValue(obj: any): void {
-    throw new Error('Method not implemented.');
+  writeValue(value: string): void {
+    this.selected = value ?? '';
   }
 
   registerOnChange(fn: Function): void {
-    throw new Error('Method not implemented.');
+    this.onChanged = fn;
   }
 
   registerOnTouched(fn: Function): void {
-    throw new Error('Method not implemented.');
+    this.onTouched = fn;
   }
 
   setDisabledState?(isDisabled: boolean): void {
-    throw new Error('Method not implemented.');
+    this.disabled = isDisabled;
   }
 }
